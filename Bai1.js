290821a66e44f673fde1afc23e0b61801583210f
DuongThiAiNhi <caocao2082004hi@gmail.com>
2024-05-09 18:49:59 +0700

    bai tap buoi 1

---
+++ Bai1.js
@@ -0,0 +1,8 @@
+let masv1=2122110524
+let hoten1=" Duong Thi Ai Nhi"
+console.log(masv1);
+console.log(hoten1);
+
+document.getElementById("show").innerHTML=`Mã sinh viên: ${masv1}   Họ tên:  ${hoten1}`;
+
+
---
+++ bai2.js
@@ -0,0 +1,14 @@
+document.getElementById("show").innerHTML=`
+<tr>
+    <td>1</td>
+    <td>2322331965</td>
+    <td>bui van luan</td>
+    <td>2435</td>
+</tr>
+<tr>
+    <td>2</td>
+    <td>2322331962</td>
+    <td>bui luan ly</td>
+    <td>2433</td>
+</tr>
+`;
